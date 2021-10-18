96b6c2c5523fac0738fad50cc9a9d9b2df0e81be
victoria-parker <victoriaparkerb@gmail.com>
2021-10-18 10:46:26 +0100

    started with on Find the Symmetric Difference, work in progress

---
+++ Algorithms/Find-the-symmetric-difference/master.js
@@ -0,0 +1,11 @@
+function sym(...args) {
+    //capturo los argumentos
+    let arr=[...args]
+  
+    for(let i=0;i<arr.length;i++){
+     
+    }
+    return arr;
+  }
+  
+  console.log(sym([1, 2, 3], [5, 2, 1, 4]));
