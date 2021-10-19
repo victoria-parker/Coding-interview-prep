1758da07695ca245453142066bef336dbf110bd0
victoria-parker <victoriaparkerb@gmail.com>
2021-10-19 19:23:33 +0100

    filter repeated numbers added, need to keep working on the finding the difference

--- Algorithms/Find-the-symmetric-difference/master.js
+++ Algorithms/Find-the-symmetric-difference/master.js
@@ -1,11 +1,17 @@
 function sym(...args) {
-    //capturo los argumentos
-    let arr=[...args]
+  let arr=[...args]
+//primero filtro los numeros repetidos
+  for(let i=0;i<arr.length;i++){
+    arr[i]=arr[i].filter(function(value,index){
+      return arr[i].indexOf(value)==index
+    })
+  }
+
+//busco la diferencia
+    let acc =arr[0].concat(arr[1]).filter(value=>!arr[0].includes(value) || !arr[1].includes(value))
   
-    for(let i=0;i<arr.length;i++){
-     
-    }
-    return arr;
+
+  return acc
   }
   
-  console.log(sym([1, 2, 3], [5, 2, 1, 4]));
+  console.log(sym([3, 3, 3, 2, 5], [2, 1, 5, 7]));
